9d189166b542d29ae6127ea3ae34a6202f2372f5
José Matioli <j-mateusalves@hotmail.com>
2022-08-01 17:29:49 -0300

    Criando rotas

---
+++ src/app/views/details/details.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetailsComponent } from './details/details.component';
+
+
+
+@NgModule({
+  declarations: [
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DetailsModule { }
