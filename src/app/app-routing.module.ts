5941a75856421122bdc298a627d8d0d9ab394844
José Matioli <j-mateusalves@hotmail.com>
2022-08-01 17:36:41 -0300

    Configurando rotas e lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { 
+    path: 'detalhes/:id',
+    loadChildren: () => import('./views/details/details.module').then(m => m.DetailsModule) },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/views/details/details.module.ts
+++ src/app/views/details/details.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsComponent } from './details/details.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', component: DetailsComponent }
+];
 
 
 @NgModule({
@@ -9,7 +13,8 @@ import { DetailsComponent } from './details/details.component';
     DetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class DetailsModule { }
